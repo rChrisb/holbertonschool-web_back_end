95b3f61174691c012b8a161089a62e35b2770f51
rChrisb <5767@holbertonstudents.com>
2023-09-25 14:10:44 +0200

    tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Tasks"""
+
+
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ return the list of all the delays n ascending order
+    without using sort() because of concurrency"""
+    the_list = []
+    delays = [wait_random(max_delay) for i in range(n)]
+    results = await asyncio.gather(*delays)
+    the_list.extend(results)
+    for i in range(len(the_list)):
+        for k in range(0, len(the_list) - i - 1):
+            if the_list[k] > the_list[k + 1]:
+                the_list[k], the_list[k + 1] = the_list[k + 1], the_list[k]
+    task_wait_random(max_delay)
+
+    return the_list

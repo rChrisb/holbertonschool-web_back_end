bdd7e1858e762910f0be040f50278d9bddc9c1a8
rChrisb <5767@holbertonstudents.com>
2023-09-26 16:14:17 +0200

    Basic dictionary

--- 0-main.py
+++ 0-main.py
@@ -1,5 +1,20 @@
-#!/usr/bin/env python3
-add = __import__('0-add').add
+#!/usr/bin/python3
+""" 0-main """
+BasicCache = __import__('0-basic_cache').BasicCache
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+my_cache = BasicCache()
+my_cache.print_cache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+print(my_cache.get("D"))
+my_cache.print_cache()
+my_cache.put("D", "School")
+my_cache.put("E", "Battery")
+my_cache.put("A", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))

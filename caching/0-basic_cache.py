d584b70fc028992bfdaef3257d75e0098562aaa5
rChrisb <5767@holbertonstudents.com>
2023-09-28 01:36:27 +0200

    Basic caching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -10,7 +10,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item in the cache"""
         if key is None or item is None:
-            pass
+            return
         self.cache_data[key] = item
 
     def get(self, key):

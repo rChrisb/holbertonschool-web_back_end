501dee60f15afc6efe065b1fe66f2549137adf5f
rChrisb <5767@holbertonstudents.com>
2023-10-01 17:45:30 +0200

    index range

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """return start_index, end_index"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index

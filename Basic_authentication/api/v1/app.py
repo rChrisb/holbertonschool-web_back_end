3a2cb73aee2641b105e4772a7d89f6cc4f3f10d7
rChrisb <5767@holbertonstudents.com>
2023-10-08 14:23:06 +0200

    Auth class

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -41,13 +41,16 @@ def before_request():
     """Before request handler"""
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if request.path not in excluded_paths and auth.require_auth(
-            request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '\
+        /api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":

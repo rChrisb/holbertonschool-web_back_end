42785d0b49d667efd5776efa3c8eb246a4bef409
rChrisb <5767@holbertonstudents.com>
2023-10-08 15:15:44 +0200

    User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,6 +48,7 @@ class BasicAuth(Auth):
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
                                  ) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
         if decoded_base64_authorization_header is None:
             return None, None
         if not isinstance(decoded_base64_authorization_header, str):

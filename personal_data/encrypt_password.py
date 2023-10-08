dcbe210ce7506d326bd58573a3f94e3bc6ea09fb
rChrisb <5767@holbertonstudents.com>
2023-10-08 13:11:08 +0200

    encryting passwords

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Encrypting passwords"""
+
+
+import bcrypt
+
+
+def hash_password(password):
+    """
+    Generate a salt and hash the password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password

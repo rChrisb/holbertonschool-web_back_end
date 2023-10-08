72330d96a9b1d5e518a1839b6867837357e479e8
rChrisb <5767@holbertonstudents.com>
2023-10-08 15:20:29 +0200

    User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,7 @@
 from typing import List, TypeVar
 from flask import request
 import base64
-
-
+from api.v1.models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -59,3 +58,23 @@ class BasicAuth(Auth):
 
         user_credentials = decoded_base64_authorization_header.split(':', 1)
         return tuple(user_credentials)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user

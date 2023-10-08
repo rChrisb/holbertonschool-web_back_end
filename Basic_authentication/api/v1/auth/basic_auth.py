4e0ad57c79ed654261a6633210a6883427a480c7
rChrisb <5767@holbertonstudents.com>
2023-10-08 15:09:52 +0200

    Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -28,3 +28,16 @@ class BasicAuth(Auth):
         base64_part = authorization_header.split(' ')[1]
 
         return base64_part
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except Exception:
+            return None

fedb093362d61b29c5d62c5c9383a9a31e0c3e38
rChrisb <5767@holbertonstudents.com>
2023-10-08 11:51:46 +0200

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -22,6 +22,7 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum"""
         message = super().format(record)
         return filter_datum(self.fields, self.REDACTION, message,
                             self.SEPARATOR)

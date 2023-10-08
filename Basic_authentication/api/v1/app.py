91f6ecc6fd55e7c38fc718696e5e83733af5f127
rChrisb <5767@holbertonstudents.com>
2023-10-08 13:40:03 +0200

    Error handler: forbidden

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -20,6 +20,12 @@ def unauthorized(error):
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error):
+    """forbidden"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler

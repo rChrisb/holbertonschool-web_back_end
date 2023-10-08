d1ba56a93715bdecf7147ebdb0e75062e9d7c7a7
rChrisb <5767@holbertonstudents.com>
2023-10-08 11:40:00 +0200

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,6 +9,7 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
-    return re.compile(r'(' + '|'.join(
+    re.compile(r'(' + '|'.join(
         re.escape(field) for field in fields) + r')=[^' + separator + r']*'
-                      ).sub(f'\\1={redaction}', message)
+                      )
+    return re.sub(f'\\1={redaction}', message)

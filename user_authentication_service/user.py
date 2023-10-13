f34b447bca8af651c5bc03776624f84c7cd83a95
rChrisb <5767@holbertonstudents.com>
2023-10-13 15:04:03 +0200

    user model

---
+++ user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""User model"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __init__(self, email, hashed_password, session_id=None,
+                 reset_token=None):
+        self.email = email
+        self.hashed_password = hashed_password
+        self.session_id = session_id
+        self.reset_token = reset_token

40b871098bd92b9b5d7bc66925b917996b470d83
rChrisb <5767@holbertonstudents.com>
2023-10-14 23:37:14 +0200

    credentials validation

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -33,3 +33,11 @@ class Auth:
             new_user = self._db.add_user(email, hashed_password)
 
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False

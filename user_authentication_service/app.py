25533d90df05a084fda5e8845692efde21ef63ea
rChrisb <5767@holbertonstudents.com>
2023-10-14 23:56:58 +0200

    logout

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -51,6 +51,17 @@ def login():
 
     return response, 200
 
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect("/")
+    else:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

b4688c08ee83776bc3789f140ba7a2e13407a9eb
rChrisb <5767@holbertonstudents.com>
2023-10-15 00:03:34 +0200

    logout

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -39,17 +39,16 @@ def register_user():
 
 @app.route('/sessions', methods=['POST'])
 def login():
-
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not AUTH.valid_login(email, password):
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = redirect("/")
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
         abort(401)
-    session_id = AUTH.create_session(email)
-    response = make_response(jsonify({"email": email, "message": "logged in"}))
-    response.set_cookie("session_id", session_id)
-
-    return response, 200
 
 
 @app.route('/sessions', methods=['DELETE'])

0ff631fa484335f321c3d400a1df6e47cd92301c
rChrisb <5767@holbertonstudents.com>
2023-10-15 16:41:00 +0200

    update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -86,7 +86,7 @@ class Auth:
 
 def update_password(self, reset_token: str, password: str) -> None:
     try:
-        user = self._db.find_user_by(reset_token=reset_token)
+        user = self._db.find_user_by(reset_token)
         hashed_password = _hash_password(password)
         user.hashed_password = hashed_password
         user.reset_token = None

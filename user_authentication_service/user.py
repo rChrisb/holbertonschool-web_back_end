01121a553fcd427f7b3ddeb1ccd3612b77fd97d8
rChrisb <5767@holbertonstudents.com>
2023-10-15 17:10:05 +0200

    user class

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -13,10 +13,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __init__(self, email, hashed_password, session_id=None,
                  reset_token=None):

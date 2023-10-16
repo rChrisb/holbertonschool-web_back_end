68f9980c980e5ea01f7a0fa7a95251dcf59c894c
rChrisb <5767@holbertonstudents.com>
2023-10-16 10:52:10 +0200

    unit tests

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -28,5 +28,62 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
 
+class TestGetJson(unittest.TestCase):
+    """
+    Test get_json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Test get_json
+        """
+        class Mocked(Mock):
+            """
+            Mocked class
+            """
+
+            def json(self):
+                """
+                Mocked json method
+                """
+                return test_payload
+
+        with patch('requests.get') as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test memoize
+    """
+
+    def test_memoize(self):
+        """
+        test memoize
+        """
+
+        class TestClass:
+            """
+            Test class
+            """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mocked:
+            spec = TestClass()
+            spec.a_property
+            spec.a_property
+            mocked.asset_called_once()
+
+
 if __name__ == '__main__':
     unittest.main()

4abc0235fec8d6d640674c8a7567e1bf659a8508
rChrisb <5767@holbertonstudents.com>
2023-10-21 15:35:22 +0200

    parametrize templates

---
+++ i18n/4-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """configurations"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale from request"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """rendering template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()

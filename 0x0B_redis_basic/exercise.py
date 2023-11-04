41a882e4e40fd55a9a0bbfddd6c74c94c0c5e44c
rChrisb <5767@holbertonstudents.com>
2023-11-04 20:48:03 +0100

    Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import redis
-from typing import Optional, Callable
+from typing import Optional, Callable, Union
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Optional[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         # Generate a random key (e.g., using uuid)
         key = "some_random_key"  # Replace this with your key generation logic
 
@@ -17,7 +17,7 @@ class Cache:
 
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[str, bytes, int, float]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
         # Retrieve data from Redis
         data = self._redis.get(key)
 
@@ -37,15 +37,4 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         return self.get(key, fn=int)
 
-# Example usage:
-cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value

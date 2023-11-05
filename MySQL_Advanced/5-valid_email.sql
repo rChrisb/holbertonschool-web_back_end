65bfeb7b2f557fdb79017ac1d98a9be508e08900
rChrisb <5767@holbertonstudents.com>
2023-11-05 09:30:57 +0100

    scripts

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- valid email
+DELIMITER $$
+
+CREATE TRIGGER new_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+	SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
---
+++ MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+-- index
+
+CREATE INDEX idx_name_first
+ON names(name(1));

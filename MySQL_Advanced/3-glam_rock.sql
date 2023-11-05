4947a729e84e0cd3f22559bac0aa5b30a71edad1
rChrisb <5767@holbertonstudents.com>
2023-11-05 09:26:41 +0100

    old school brand

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,11 +1,6 @@
 -- old school band
 
-SELECT band_name, 
-       CASE
-            WHEN formed IS NOT NULL AND split IS NOT NULL THEN EXTRACT(YEAR FROM split) - EXTRACT(YEAR FROM formed)
-            WHEN formed IS NOT NULL THEN EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM formed)
-            ELSE NULL
-       END AS lifespan
-FROM metal_bands
-WHERE band_style = 'Glam rock'
-ORDER BY lifespan DESC;
+SELECT DISTINCT band_name,
+                IFNULL(`split`, 2020) - formed as lifespan
+    FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
+    ORDER BY `lifespan` DESC;

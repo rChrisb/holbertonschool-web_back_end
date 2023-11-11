e2069e7962437b08b405d0c8e3ab53b01ab0c208
rChrisb <5767@holbertonstudents.com>
2023-11-11 21:47:36 +0100

    no more var in js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task3 = true;
-    const task22 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];

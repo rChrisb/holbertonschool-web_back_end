e818d5d3839e38dc05754451929d393d28958a86
rChrisb <5767@holbertonstudents.com>
2023-11-11 21:46:24 +0100

    no more var in js

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task3 = true;
+    const task22 = false;
+  }
+
+  return [task, task2];
+}

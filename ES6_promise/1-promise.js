d23cf5225372a9c51a8985925e4534909ad992a0
rChrisb <5767@holbertonstudents.com>
2023-11-19 00:01:55 +0100

    promise function

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const response = {
+        status: 200,
+        body: "Success",
+      };
+      resolve(response);
+    } else {
+      const error = new Error("The fake API is not working currently");
+      reject(error);
+    }
+  });
+}

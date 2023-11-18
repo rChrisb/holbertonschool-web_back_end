7fe0048be225cc07defac371ff46057875e8c397
rChrisb <5767@holbertonstudents.com>
2023-11-18 23:57:30 +0100

    promise function

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,16 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call or any asynchronous operation
+    const isSuccess = Math.random() < 0.5; // For demonstration purposes
+
+    // Resolve the promise if the operation is successful
+    if (isSuccess) {
+      const responseData = { message: "API call successful" };
+      resolve(responseData);
+    } else {
+      // Reject the promise if there is an error
+      const error = new Error("API call failed");
+      reject(error);
+    }
+  });
+}

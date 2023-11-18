183d947f25f0ac3aad1012cde66550bfc4b269fb
rChrisb <5767@holbertonstudents.com>
2023-11-19 00:48:22 +0100

    promise function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,22 +1,16 @@
 /* eslint-disable */
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
 
-export default /*async */ function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName
-) {
-  const user = signUpUser();
-  const photo = uploadPhoto();
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
 
-  return Promise.allSettled([user, photo]).then((values) => {
-    values.map((v) => ({
-      status: v.status,
-      value:
-        v.status === "fulfilled"
-          ? v.value
-          : `${v.reason.name}: ${v.reason.message}`,
-    }));
-  });
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === "fulfilled" ? result.value : result.reason,
+    }))
+  );
 }

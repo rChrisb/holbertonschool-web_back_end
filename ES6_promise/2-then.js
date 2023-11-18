fe8b57dd90fd09f1c5af6967eefe625242478e89
rChrisb <5767@holbertonstudents.com>
2023-11-19 00:10:15 +0100

    promise function

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
+    .then((response) => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
-    .catch(() => {
+    .catch((error) => {
       console.error('Got an error from the API');
       return new Error();
     });

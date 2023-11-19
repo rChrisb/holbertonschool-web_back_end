890b9ec0fc23617d6ee2e1346f5f00f805b31306
rChrisb <5767@holbertonstudents.com>
2023-11-19 01:45:31 +0100

    es6 classes

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,24 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    // Create a new instance of the Car class with the same attributes
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
